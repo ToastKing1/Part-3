a665b208c643722c2f2bbccd39e024d1e14279de
ToastKing1 <owenspreadbury1@gmail.com>
2024-03-13 10:06:04 -0400

    added merchant script

---
+++ Assets/Week 9/Scripts/Merchant.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Merchant : Villager
+{
+    // hmrt
+    // hmmmmmmmmmmm
+    // hmmr
+    // hmm
+    // 16 emeralds for diamond sword with fire aspect II
+
+    // some may call this junk but I call them treasures
+    // Take a look
+    // got some bits and pieces out here, more inside
+    // Have you heard of the High Elves? Haah, Burgh, Hrmhf (dead from poison apple)
+    // (ignore this nonsense above I'm just being silly
+
+
+}
