d6bf881ef8726fac88b54282e471f9efe1734b53
ToastKing1 <owenspreadbury1@gmail.com>
2024-03-13 12:29:09 -0400

    task4b complete: ui text switches between villagers or none if none are selected

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,18 +1,29 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
+    public static TextMeshProUGUI villagerTypeText;
+
+    public void Start()
+    {
+        villagerTypeText = FindObjectOfType<TextMeshProUGUI>();
+    }
+
     public static Villager SelectedVillager { get; private set; }
     public static void SetSelectedVillager(Villager villager)
     {
         if(SelectedVillager != null)
         {
             SelectedVillager.Selected(false);
+            villagerTypeText.text = "None";
         }
         SelectedVillager = villager;
+        villagerTypeText.text = SelectedVillager.GetType().Name;
         SelectedVillager.Selected(true);
     }
     
